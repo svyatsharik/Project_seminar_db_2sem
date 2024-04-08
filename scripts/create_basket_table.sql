07cc06662b3e2961684dd38bfd44ec83e1c7b019
Святослав Шаров <sharov.so@phystech.edu>
2024-04-08 20:03:51 +0000

    Delete create_basket_table.sql

--- scripts/create_basket_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE basket
-(
-    basketID INTEGER UNIQUE PRIMARY KEY NOT NULL,
-    buyerID INTEGER NOT NULL,
-    productID INTEGER NOT NULL,
-    count INTEGER NOT null,
-    FOREIGN KEY (buyerID) REFERENCES buyer (buyerID),
-    FOREIGN KEY (productID) REFERENCES product (productID)
-);
