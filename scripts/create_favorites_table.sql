b05c22072d22568a0a0bda4a3b8619d194ddfd39
Святослав Шаров <sharov.so@phystech.edu>
2024-04-08 20:04:08 +0000

    Delete create_favorites_table.sql

--- scripts/create_favorites_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE favorites
-(
-    favoritesID INTEGER UNIQUE PRIMARY KEY NOT NULL,
-    buyerID INTEGER NOT NULL,
-    productID INTEGER NOT NULL,
-    count INTEGER NOT NULL,
-    FOREIGN KEY (buyerID) REFERENCES buyer (buyerID),
-    FOREIGN KEY (productID) REFERENCES product (productID)
-);
