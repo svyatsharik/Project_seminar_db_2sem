65345a1f3f164604fd5f860c6b295bcc17cb8254
Святослав Шаров <sharov.so@phystech.edu>
2024-04-08 20:04:12 +0000

    Delete create_product_table.sql

--- scripts/create_product_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE product
-(
-    productID INTEGER UNIQUE PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    price FLOAT NOT NULL,
-    categoryID INTEGER NOT NULL,
-    count INTEGER NOT NULL,
-    FOREIGN KEY (categoryID) REFERENCES category (categoryID)
-);
