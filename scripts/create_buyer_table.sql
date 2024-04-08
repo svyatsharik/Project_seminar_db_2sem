a2626627001dc540efbda713b3c5ded21e5be9db
Святослав Шаров <sharov.so@phystech.edu>
2024-04-08 20:03:56 +0000

    Delete create_buyer_table.sql

--- scripts/create_buyer_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE buyer
-(
-    buyerID INTEGER UNIQUE PRIMARY KEY NOT NULL,
-    firstName VARCHAR(100) NOT NULL,
-    lastName VARCHAR(100) NOT NULL,
-    phoneNumber VARCHAR(15) NOT NULL,
-    address VARCHAR(200) NOT NULL
-);
