f09f7de0f5f170ad8795a3277de05fa772b00d28
Святослав Шаров <sharov.so@phystech.edu>
2024-04-22 22:18:10 +0000

    Update select_scripts.sql

--- scripts/select_scripts.sql
+++ scripts/select_scripts.sql
@@ -19,3 +19,35 @@ select name, categoryid
 from product
 where price > 5000
 order by categoryid;
+
+select name, price, categoryid, sum(price) over w as s
+from product
+window w as (
+partition by categoryid
+)
+order by s;
+
+select name, price, categoryid, avg(price) over w, (price - avg(price) over w) as delta
+from product
+window w as (
+partition by categoryid
+);
+
+select rank() over w, buyerid, productid, count
+from basket
+window w as (
+order by buyerid
+);
+
+select row_number() over w, name, categoryid, price
+from product
+window w as (
+order by categoryid, price
+);
+
+select name, categoryid, price, lag(price, 1) OVER w, (price - lag(price, 1) OVER w) as delta
+from product
+WINDOW w AS (
+order by price
+)
+order by price;
